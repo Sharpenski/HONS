eab584bb14393676feb314d6611a04f9c2ca73bd
Sharpenski <dobbst1994@gmail.com>
2016-01-14 15:26:01 +0000

    Construction time again

--- MLP/MLP.py
+++ MLP/MLP.py
@@ -17,16 +17,17 @@ class MLP:
     
     def __init__(self, file_name):
         self.readParam(file_name)
-        self.neurons = []
+        self.layers = []
         
     def addLayer(self):
-        self.noLayers += 1
+        self.layers.append(Neuron_Layer())
+        print "A new layer was added to the MLP."
         
     def readParam(self, file_name):
         print file_name
         
     def getInfo(self):
-        print ("The MLP consists of " + str(self.noLayers) + " layers")
+        print ("The MLP consists of " + str(len(self.layers)) + " layers"), self.layers
         
 #===============================================================================
 # Neuron_Layer: represents a layer within an MLP
@@ -34,14 +35,20 @@ class MLP:
 class Neuron_Layer:
     
     def __init__(self):
-        self.neurons = [] # stores each Neuron within the layer
+        self.neurons = [] # stores each Neuron within the layer (also used to find width)
         
     def addNeuron(self, toAdd):
         self.neurons.append(toAdd)
         print "Added a new Neuron to the layer"
         
     def printLayer(self):
-        print self.neurons, len(self.neurons)
+        print self.neurons, len(self.neurons) 
+        
+    def __str__(self):
+        return "Neuron_Layer"
+    
+    def __repr__(self):
+        return "Neuron_Layer"
         
 #===============================================================================
 # Neuron: Represents an individual Neuron
@@ -51,19 +58,33 @@ class Neuron:
     def __init__(self):
         self.weights = [] # stores the weight value of each incoming connection
         
+    def init_weights(self):
+        print
+        
     def assignActivation(self, func_name):
         self.function = activation.getFunc(func_name)
         
     def printNeuron(self):
         print "The activation function of the Neuron is:", self.function.__name__
         
+    def __str__(self):
+        return "Neuron string rep"
+    
+    def __repr__(self):
+        return "Neuron string rep"
+    
 #===============================================================================
 # back_propagate: train the network using back propagation
 #===============================================================================
 def back_propagate(learning_rate, momentum_factor):
     
     print learning_rate, momentum_factor
-          
+    
+    #===========================================================================
+    # calc_error: calculate the error at a particular node
+    #===========================================================================
+    def calc_error(node_name):
+        return       
         
 #===============================================================================
 # main: program called from here
@@ -73,13 +94,19 @@ def main():
     
     mlp1 = MLP("test_file.txt") 
     mlp1.getInfo()
-    
-    nl1 = Neuron_Layer(8)
-    nl1.printLayer()
+    mlp1.addLayer()
+    print str(mlp1.layers[0])
+    mlp1.addLayer()
+    mlp1.addLayer()
+    mlp1.getInfo()
     
     n1 = Neuron()
+    print n1
     n1.assignActivation("sigmoid")
     n1.printNeuron()
+    
+    nl1 = Neuron_Layer()
+    print nl1
     
     nl1.addNeuron(n1)
     
